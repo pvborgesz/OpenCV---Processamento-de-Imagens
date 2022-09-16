e7c5a43482ddba432d435961d6fdd1428734e3e1
Paulo Victor Borges <pvbo.lima@discente.ufma.br>
2022-09-16 14:22:57 -0300

    Adicionando contador de tempo de execução

--- main.py
+++ main.py
@@ -2,45 +2,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv 
 
-img = cv.imread('images/elephant.jpg', 0) #Lendo imagem
+import time
+start_time = time.time()
 
 
-plt.subplot(121),plt.imshow(img),plt.title('Original')
-plt.xticks([]), plt.yticks([])
-plt.show()
+def imgAnalyzer(imgAddress):
 
-size = np.size(img)
-skel = np.zeros(img.shape,np.uint8)
- 
-ret,img = cv.threshold(img,127,255,0)
-element = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
-done = False
+    img = cv.imread(imgAddress, 0) #Lendo imagem
 
-plt.subplot(121),plt.imshow(img),plt.title('Shape of img ')
 
-plt.show()
+    plt.subplot(121),plt.imshow(img),plt.title('Original')
+    plt.xticks([]), plt.yticks([])
+    plt.show()
 
-cont = 0
+    size = np.size(img)
+    skel = np.zeros(img.shape,np.uint8)
+    
+    ret,img = cv.threshold(img,127,255,0)
+    element = cv.getStructuringElement(cv.MORPH_CROSS,(3,3))
+    done = False
 
-while( not done):
-    eroded = cv.erode(img,element)
-    temp = cv.dilate(eroded,element)
-    temp = cv.subtract(img,temp)
-    skel = cv.bitwise_or(skel,temp)
-    img = eroded.copy()
- 
-    zeros = size - cv.countNonZero(img)
-    if zeros==size:
-        done = True
- 
+    plt.subplot(121),plt.imshow(img),plt.title('Shape of img ')
+
+    plt.show()
+
+    cont = 0
+
+    while( not done):
+        eroded = cv.erode(img,element)
+        temp = cv.dilate(eroded,element)
+        temp = cv.subtract(img,temp)
+        skel = cv.bitwise_or(skel,temp)
+        img = eroded.copy()
+    
+        zeros = size - cv.countNonZero(img)
+        if zeros==size:
+            done = True
+        cont+=1
     plt.imshow(skel,cmap="gray"), plt.title("Img in iteration {}".format(cont))
     plt.xticks([]), plt.yticks([])
     plt.show()
-    cont += 1 
-# cv.imshow("skel",skel)
-# cv.waitKey(0)
-# cv.destroyAllWindows()
+        
+    # print(cont)
 
 
-# plt.subplot(121),plt.imshow(skel),plt.title('Após bitwise or')
-# plt.xticks([]), plt.yticks([])
+imgAnalyzer('images/horse.jpg')
+
+print("--- Tempo de execucao de: %s segundos ---" % (time.time() - start_time))
